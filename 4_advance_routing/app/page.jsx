785ca09511a5da3b9c1a9675b2b77e9bba15ebf4
wespy <wespy07@gmail.com>
2025-04-25 15:37:26 +0530

    working clean

---
+++ 4_advance_routing/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center items-center bg-gray-800 text-white">
+        <div className="bg-emerald-700 flex items-center justify-center h-20 w-40"></div>
+      </div>
+    </>
+  );
+}
